e3b55b426b6a846ca290202877b5fbd06dde3d4e
Hwa <132264214+hwm31@users.noreply.github.com>
2024-11-22 23:38:27 +0900

    friend_list api 연결 수정

--- friend_list.py
+++ friend_list.py
@@ -1,8 +1,10 @@
 from flask import Flask, request
 import pymysql
 import pandas as pd
+from flask_cors import CORS
 
 app = Flask(__name__)
+CORS(app)
 
 @app.route('/friend_list', methods=['POST'])
 def friend_list_query():
